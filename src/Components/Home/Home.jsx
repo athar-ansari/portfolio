41eb422bc12be7439fcbf641037f400696936601
Athar Ansari <athar8932@gmail.com>
2024-02-12 11:10:10 +0530

    first commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <>
       <NavBar />
-      <div className="homeSection h-screen w-full bg-[#f5f5f5] text-black xs:overflow-hidden">
+      <div className="homeSection h-screen w-full bg-[#f5f5f5] text-black xs:h-fit">
         <div className="homeContainer ">
           <div className="homeContent  ml-44 pt-20  gap-x-8 	items-center xl:ml-32 lg:ml-7 lg:gap-x-5 grid md:block	 xs:gap-x-1  xs:ml-32">
             <HomeSocial />
